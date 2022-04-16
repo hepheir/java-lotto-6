22698a195244861947f38c1311f3cc1ca785648f
Hepheir <hepheir@gmail.com>
2022-04-17 03:18:55 +0900

    docs(domain/LotteryTicketBuilder): remove outdated comment

--- src/main/java/domain/LotteryTicketBuilder.java
+++ src/main/java/domain/LotteryTicketBuilder.java
@@ -1,8 +1,5 @@
 package domain;
 
 public interface LotteryTicketBuilder extends Builder<LotteryTicket> {
-    /**
-     * 체인으로 사용할 수 있도록 함.
-     */
     void addNumber(LotteryNumber number) throws IndexOutOfBoundsException;
 }
