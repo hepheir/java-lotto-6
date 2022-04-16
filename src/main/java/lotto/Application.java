1f7a5d951cbc0093a1e37a534f3490f2561d25f7
Hepheir <hepheir@gmail.com>
2022-04-17 01:52:07 +0900

    feat(Main): write skeleton code with interfaces

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,67 @@
 package lotto;
 
+import java.util.Arrays;
+
+import controllers.Controller;
+import domain.Driver;
+import domain.LotteryGrade;
+import domain.LotteryGrader;
+import domain.LotteryNumber;
+import domain.LotteryNumberSupplier;
+import domain.LotteryTicket;
+import domain.LotteryTicketBuilder;
+import domain.TicketGroup;
+import utils.Analytics;
+import views.View;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Controller controller = null;
+        View view = null;
+        Driver driver = null;
+        LotteryTicketBuilder ticketBuilder = null;
+        LotteryNumberSupplier manualNumberSupplier = null;
+        LotteryNumberSupplier autoNumberSupplier = null;
+        Analytics<LotteryGrade> analytics = null;
+        TicketGroup<LotteryTicket> ticketGroup = null;
+        LotteryGrader grader = null;
+
+        view.printEnterPurchaseAmount();
+        Integer money = controller.readInteger();
+        Integer countOfBuyableTickets = money / LotteryTicket.PRICE;
+
+        view.printEnterNumberOfLotteryTicketsToManuallySetNumber();
+        Integer countOfManuallyCreatedTickets = controller.readInteger();
+        Integer countOfAutomaticallyCreatedTickets = countOfBuyableTickets - countOfManuallyCreatedTickets;
+
+        view.printEnterNumbersToSet();
+        for (int i = 0; i < countOfManuallyCreatedTickets; i++) {
+            ticketGroup.addTicket(driver.createLotteryTicket(ticketBuilder, manualNumberSupplier));
+        }
+        for (int i = 0; i < countOfAutomaticallyCreatedTickets; i++) {
+            ticketGroup.addTicket(driver.createLotteryTicket(ticketBuilder, autoNumberSupplier));
+        }
+        view.printPurchaseReport(countOfManuallyCreatedTickets, countOfAutomaticallyCreatedTickets);
+        ticketGroup.getTickets().stream().map(LotteryTicket::toString).forEach(view::printLine);
+
+        view.printEnterWinningNumbersOfLastWeek();
+        LotteryTicket winningNumbers = driver.createLotteryTicket(ticketBuilder, manualNumberSupplier);
+
+        view.printEnterBonusBall();
+        LotteryNumber bonusNumber = driver.createLotteryNumber(manualNumberSupplier);
+
+        grader.setWinningNumbers(winningNumbers);
+        grader.setBonusNumber(bonusNumber);
+
+        view.printWinningAnalytics();
+        ticketGroup.getTickets().stream().map(grader::grade).forEach(analytics::add);
+
+        Arrays.asList(LotteryGrade.values())
+                .forEach(grade -> view.printGradeAndPrizeAndQuantity(
+                        analytics.getLabel(grade),
+                        analytics.getValue(grade),
+                        analytics.getQuantity(grade)));
+
+        view.printProfitReport(analytics.getTotalValue().doubleValue() / money.doubleValue());
     }
 }
