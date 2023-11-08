0d9196f52698f529a6fa385270241e17a4cf0d3b
Hepheir <hepheir@gmail.com>
2023-11-08 23:48:27 +0900

    refactor(Lotto): 으아아 급한 기능 추가~ 뾰옹

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,11 @@
 package lotto;
 
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
+    public static final int PRICE = 1000;
+    public static final int QUANTITY_OF_NUMBERS = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -10,11 +13,32 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public List<Integer> getNumbers() {
+        return Collections.unmodifiableList(numbers);
+    }
+
     private void validate(List<Integer> numbers) {
+        validateHasSixNumbers(numbers);
+        validateHasDifferentNumbers(numbers);
+    }
+
+    // TODO: 추가 기능 구현
+
+    private void validateHasSixNumbers(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateHasDifferentNumbers(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<Integer>(numbers);
+        if (numbers.size() != uniqueNumbers.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
 }
